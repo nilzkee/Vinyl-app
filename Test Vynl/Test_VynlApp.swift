35cfc06481a3e29da2006915282eb90eaa1836b1
nilzkee <168503667+nilzkee@users.noreply.github.com>
2024-05-11 00:53:55 +0700

    Initial Commit

---
+++ Test Vynl/Test_VynlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_VynlApp.swift
+//  Test Vynl
+//
+//  Created by Nila Eleora Putri Sianturi on 11/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_VynlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
